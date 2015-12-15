5c256200bec05d8529523ad5f14787f71440f216
Stu Kabakoff <stuart@compstak.com>
2015-12-15 18:03:07 -0500

    got it working!

--- setup.sh
+++ setup.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-if [ -d ./.git/info ]; then
-	echo 'not making ./git/info'
+if [ -d ../../.git/info ]; then
+	echo 'not making ../.git/info'
 else
-	mkdir ./.git/info
+	mkdir ../../.git/info
 fi
 
-if [ -f ./.git/info/attributes ]; then
-	echo 'not making ./git/info/attributes'
+if [ -f ../../.git/info/attributes ]; then
+	echo 'not making ../.git/info/attributes'
 else
 	attributes="
-\n*.js  filter=repoformat
-\n*.jsx  filter=repoformat
+\n*.js filter=repoformat
+\n*.jsx filter=repoformat
 "
 
-	echo -e $attributes >> ./.git/info/attributes
+	echo -e $attributes >> ../../.git/info/attributes
 fi
 
-Smudge=$(git config filter.repoformat.smudge)
+git config filter.repoformat.smudge './node_modules/repo-format/smudge %f'
+git config filter.repoformat.clean './node_modules/repo-format/clean %f'
 
-echo $Smudge;
+echo 'Installed repo-format, you may need to run git checkout HEAD -- "$(git rev-parse --show-toplevel)" to see the changes.'
 
-if [ "$Smudge" == '' ]; then
-	git config filter.repoformat.smudge 'test ./local-format.json && ./node_modules/repo-format/node_modules/.bin/esformatter --config ./local-format.json || cat'
-	git config filter.repoformat.clean './node_modules/repo-format/node_modules/.bin/esformatter --config ./repo-format.json'
-
-	git checkout HEAD -- **
-else
-	echo 'not adding git settings'
-fi
